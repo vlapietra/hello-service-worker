fb6d4f8abb07c7ae9a0ce5e69ea04cfd698c5006
Vinny LaPietra <vinlapietra@yahoo.com>
2017-11-25 08:06:15 -0500

    add JS and CSS to cache storage

--- sw.js
+++ sw.js
@@ -1,13 +1,28 @@
 
-const VERSION = 'v2';
+const VERSION = 'v3';
 
-self.addEventListener('install', event => {
-    log("INSTALLING ");
-    const installCompleted = Promise.resolve()
-                        .then(() => log("INSTALLED"));
+let getCacheName = () => `app-cache-${VERSION}`;
 
-    event.waitUntil(installCompleted);
-});
+self.addEventListener('install', event => event.waitUntil(installServiceWorker()));
+
+
+async function installServiceWorker() {
+
+    log("Service Worker installation started ");
+
+    const cache = await caches.open(getCacheName());
+
+    return cache.addAll([
+        '/',
+        'carousel.css',
+        'http://getbootstrap.com/dist/css/bootstrap.min.css',
+        'https://code.jquery.com/jquery-3.2.1.slim.min.js',
+        'http://getbootstrap.com/assets/js/vendor/popper.min.js',
+        'http://getbootstrap.com/dist/js/bootstrap.min.js',
+        'http://getbootstrap.com/assets/js/vendor/holder.min.js'
+    ]);
+}
+
 
 self.addEventListener('activate', event => {
     log("ACTIVATING");
